39416d40bd7c0671be685e57d741b4cb103c3050
wyjeon <wyjeon.info@gmail.com>
2021-02-21 15:08:12 +0900

    스토어 만들기

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// 1. createStore와 루트 리듀서 불러오기
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+
+// 2. 스토어 만들고 현재 값 확인 해보기
+const store = createStore(rootReducer);
+console.log(store.getState());
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
