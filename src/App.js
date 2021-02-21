3f9826d8c2e6179c69fc2daee208429e64916d6d
wyjeon <wyjeon.info@gmail.com>
2021-02-21 16:44:40 +0900

    WaitingListContainer 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import WaitingList from './components/WaitingList';
 import PaletteContainer from './containers/PaletteContainer'; // **** (1) 불러오기
 import CounterContainer from './containers/CounterContainer'; // **** (1) 불러오기
+import WaitingListContainer from './containers/WaitingListContainer'; // **** 불러오기
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <PaletteContainer /> {/* **** (2) 대체하기 */}
         <CounterContainer /> {/* ****(2) 대체하기 */}
-        <WaitingList />
+        <WaitingListContainer /> {/* **** 교체하기 */}
       </div>
     );
   }
